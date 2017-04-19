aec49319eece32e73ab34f41ed0e8f8612888cb7
Adong520 <A18758@hotmail.com>
2017-04-20 00:04:06 +1000

    finished blog comments features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,11 @@ include ArticlesHelper
 
 	def show
 		@article = Article.find(params[:id])
+
+		@comment = Comment.new
+		#@comment = @article.comments.new
+		@comment.article_id = @article.id
+
 	end
 
 	def new
